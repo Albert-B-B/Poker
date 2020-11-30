cbd4ad92b156684203a65a5e24bb71db99622165
Albert-B-B <55802313+Albert-B-B@users.noreply.github.com>
2020-11-30 20:12:08 +0100

    Basic setup

---
+++ Client.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 30 20:11:11 2020
+
+@author: albert.b.software@gamil.com
+"""
---
+++ Server.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 30 20:11:29 2020
+
+@author: albert.b.software@gamil.com
+"""
