cc4350b7ddc7e98cbcca1997ea87c00fdb6e8e4c
abj2909 <60501163+abj2909@users.noreply.github.com>
2020-12-03 13:23:48 +0100

    straight flush, two pairs

--- scoring_hands.py
+++ scoring_hands.py
@@ -1,10 +1,8 @@
 from random import randint
 
-suits = ['H','S','C','D']
-
 def build_deck():
     numbers = list(range(2, 15))
-    global suits
+    suits = ['H','S','C','D']
     deck = []
     for i in numbers:
         for s in suits:
@@ -23,8 +21,54 @@ def draw(deck, amount):
 
     return drawnCards
 
+def strflush(hand, table):
+    suits = ['H','S','C','D']
+    handTableSuit = table + hand
+    
+    count = 0
+    for suit in suits:
+        for i in range(0,6):
+            if suit + str(i) in handTableSuit:
+                count += 1
+                if suit + str(i + 1) in handTableSuit:
+                    count += 1
+                    if suit + str(i + 2) in handTableSuit:
+                        count += 1
+                        if suit + str(i +3) in handTableSuit:
+                            count += 1
+                            if suit + str(i + 4) in handTableSuit:
+                                count += 1
+                                return True
+
+def straight(hand, table):
+    handCopy = hand.copy()
+    tableCopy = table.copy()
+
+    for i in range(0,2):
+        handCopy[i] = handCopy[i][1:]
+        handCopy[i] = int(handCopy[i])
+    for i in range(0,4):
+        tableCopy[i] = tableCopy[i][1:]
+        tableCopy[i] = int(tableCopy[i])
+    handTable = tableCopy + handCopy
+
+    count = 0
+    for i in range(0,6):
+        if i in handTable:
+            count += 1
+            if i + 1 in handTable:
+                count += 1
+                if i + 2 in handTable:
+                    count += 1
+                    if i +3 in handTable:
+                        count += 1
+                        if i+4 in handTable:
+                            count += 1
+                            return True
+    return False
+
 def flush(hand, table):
-    global suits
+    suits = ['H','S','C','D']
     for i in suits:
         count = 0
         for string in table:
@@ -55,6 +99,37 @@ def OAC(hand, table, amount):
             return True
     return False
 
+def highCard(hand):
+    handCopy = hand.copy()
+    for i in range(0,2):
+        handCopy[i] = handCopy[i][1:]
+        handCopy[i] = int(handCopy[i])
+    a = max(handCopy)
+    return a
+
+def twopairs(hand, table):
+    pair = False
+    pair2 = False
+    handCopy = hand.copy()
+    tableCopy = table.copy()
+    for i in range(0,2):
+        handCopy[i] = handCopy[i][1:]
+        handCopy[i] = int(handCopy[i])
+    for i in range(0,4):
+        tableCopy[i] = tableCopy[i][1:]
+        tableCopy[i] = int(tableCopy[i])
+    for i in range(2,15):
+        count = handCopy.count(i) + tableCopy.count(i)
+        
+        if count == 2 and not pair:
+            pair = True
+        elif count == 2:
+            pair2 = True
+
+    if pair and pair2:
+        return True
+
+
 def fullHouse(hand, table):
     pair = False
     three = False
@@ -79,37 +154,67 @@ def fullHouse(hand, table):
     
     return False
 
-deck = build_deck()
-hand = draw(deck, 2)
-table = draw(deck, 5)
-
-hand = ['D11','C11']
-table = ['H11','C2','H2','C5']
-
-print("Your hand is", hand)
-print("On the table is", table)
 
 
-if flush(hand, table) == True:
-    print("You have a flush")
+OACf = 0
+fullH = 0
+fls = 0
+strt = 0
+OACth = 0
+tOACt = 0
+OACt = 0
+hc = 0
+sf = 0
 
-elif fullHouse(hand, table) == True:
-    print("You have full house")
+#hand = ['D2','D3']
+#table = ['D4','D5','D6','C4']
 
-elif OAC(hand, table, 4) == True:
-    print("You have four of a kind!")
+#print("Your hand is", hand)
+#print("On the table is", table)
 
-elif OAC(hand, table, 3) == True:
-    print("You have three of a kind")
+#straight flush
+for i in range(0,1):
 
-elif OAC(hand, table, 2) == True:
-    print("You have a pair!")
+    deck = build_deck()
+    hand = draw(deck, 2)
+    table = draw(deck, 5)
+    if strflush(hand, table) == True:
+        print("You have a straight flush! Very cool!")
+        sf += 1
+    
+    elif OAC(hand, table, 4) == True:
+        print("You have four of a kind!")
+        OACf += 1
 
+    elif fullHouse(hand, table) == True:
+        print("You have full house")
+        fullH += 1
 
+    elif flush(hand, table) == True:
+        print("You have a flush")
+        fls += 1
 
+    elif straight(hand, table) == True:
+        print("You have a straight")
+        strt += 1
 
+    elif OAC(hand, table, 3) == True:
+        print("You have three of a kind")
+        OACth += 1
 
+    elif twopairs(hand, table) == True:
+        print("You have two pairs")
+        tOACt += 1
 
+    elif OAC(hand, table, 2) == True:
+        print("You have a pair!")
+        OACt += 1
+
+    else:
+        print("You have a high card with a value of", highCard(hand))
+        hc += 1
+
+print('Straight flushes:', sf, 'Four of a kind:',OACf, 'Full House:', fullH, 'flush:',fls,'straight:', strt,'three of a kind:', OACth, 'two pairs:', tOACt, 'pairs:', OACt, 'no hand/high card:',hc)
 
 
 
