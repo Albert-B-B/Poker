6d4ffd9b48c601920eb2a4471623176d6ff2d0ae
abj2909 <60501163+abj2909@users.noreply.github.com>
2020-12-08 19:20:17 +0100

    now works with up to 23 players

--- client_ui.py
+++ client_ui.py
@@ -2,6 +2,17 @@ from random import randint
 from tkinter import *
 from PIL import Image, ImageTk
 
+scores = []
+results = []
+
+windowX = 500
+windowY = 500
+
+drawnCards = []
+timesDrawn = 0
+
+playerAmount = int(input("How many players? "))
+
 def build_deck():
     #builds the deck with cards, all following the format Suit - number. Ace is 14, king is 13 etc
     numbers = list(range(2, 15))
@@ -164,67 +175,105 @@ def fullHouse(hand, table):
     
     return False
 
-windowX = 500
-windowY = 500
-
-drawnCards = []
-timesDrawn = 0
 
 def clicked():
 
-    global t1, t2, t3, t4, t5, cards, timesDrawn, deck, table
+    global t1, t2, t3, t4, t5, cards, timesDrawn, deck, table, hands
     timesDrawn += 1
     if timesDrawn == 1:
         #river
         cards = [table[0], table[1], table[2]]
         t1.configure(image=table1)
+        t1.place(x=20,y=150)
         t2.configure(image=table2)
+        t2.place(x=120,y=150)
         t3.configure(image=table3)
+        t3.place(x=220,y=150)
         
     elif timesDrawn == 2:
         cards.append(table[timesDrawn + 1])
         t4.configure(image=table4)
+        t4.place(x=320,y=150)
 
     elif timesDrawn == 3:
         cards.append(table[timesDrawn + 1])
         t5.configure(image=table5)
+        t5.place(x=420,y=150)
+
+        handScore.configure(text=scoreCheck(hands, table))
+            
+        handScore.place(x=150,y=265)
+
+def scoreCheck(hands, table):
+    global results
+    global scores
+    a = len(hands)
+    
+    for i in range(0, a):
+        hand = hands[i]
         
         if strflush(hand, table) == True:
-            handScore.configure(text="You have a straight flush! Very cool!")
+            scores.append(22)
+            results.append("You have a straight flush! Very cool!")
 
         elif OAC(hand, table, 4) == True:
-            handScore.configure(text="You have four of a kind!")
+            scores.append(21)
+            results.append("You have four of a kind!")
 
         elif fullHouse(hand, table) == True:
-            handScore.configure(text="You have full house")
-
+            scores.append(20)
+            results.append( "You have full house")
+        
         elif flush(hand, table) == True:
-            handScore.configure(text="You have a flush")
+            scores.append(19)
+            results.append( "You have a flush")
 
         elif straight(hand, table) == True:
-            handScore.configure(text="You have a straight")
+            scores.append(18)
+            results.append("You have a straight")
 
         elif OAC(hand, table, 3) == True:
-            handScore.configure(text="You have three of a kind")
+            scores.append(17)
+            results.append("You have three of a kind")
 
         elif twopairs(hand, table) == True:
-            handScore.configure(text="You have two pairs")
+            scores.append(16)
+            results.append("You have two pairs")
 
         elif OAC(hand, table, 2) == True:
-            handScore.configure(text="You have a pair!")
+            #handScore.configure(text="You have a pair!")
+            scores.append(15)
+            results.append("You have a pair")
 
         else:
             highCardPrint = "You have a high card with a value of " + str(highCard(hand))
-            handScore.configure(text=highCardPrint)
+            scores.append(highCard(hand))
+            results.append(highCardPrint)
+    end(scores, results, a)
+def hands(playerAmount):
+    global deck
+    a = []
+    for i in range(0,playerAmount):
+        b = draw(deck, 2)
+        a.append(b)
+    return a
 
 def Bet():
     Bet = 0
     try:
         Bet = int(txt.get())
-        betError.configure(text="")
+        betError.configure(betError.place_forget())
+        
     except ValueError:
         betError.configure(text="Your bet must be a valid int")
+        betError.place(x=1,y=375)
     return  Bet
+
+def end(scores, results, playerAmount):
+    Winner = scores.index(max(scores)) + 1
+    print("The winner was player", Winner)
+    print(scores)
+    print(results)
 
 def Check():
     print(0)
@@ -243,40 +292,68 @@ def load(lst, nr):
     return a
 
 deck = build_deck()
-hand = draw(deck, 2)
+hands = hands(playerAmount)
 table = draw(deck, 5)
+print(hands)
 
 root = Tk()
 root.geometry(str(windowX)+'x'+str(windowY))
 root.title("Poker")
 
-hand1 = load(hand, 1)
+background_image = ImageTk.PhotoImage(Image.open("Assets/backdrop.png"))
+background_label = Label(root, image=background_image)
+background_label.place(x=-2, y=-2)
+
+hand1 = load(hands[0], 1)
 h1 = Label(root, image = hand1)
 h1.place(x=140,y=335)
 
-hand2 = load(hand, 2)
+hand2 = load(hands[0], 2)
 h2 = Label(root, image=hand2)
 h2.place(x=200,y=335)
+if playerAmount >= 2:
+    backImg = ImageTk.PhotoImage(Image.open("Assets/back.png"))
+    ch1 = Label(root, image= backImg)
+    ch1.place(x=140,y=20)
+
+    ch2 = Label(root, image= backImg)
+    ch2.place(x=160,y=20)
+
+if playerAmount >= 3:
+    ch3 = Label(root, image= backImg)
+    ch3.place(x=240,y=20)
+
+    ch4 = Label(root, image= backImg)
+    ch4.place(x=260,y=20)
+
+if playerAmount >= 4:
+    ch5 = Label(root, image= backImg)
+    ch5.place(x=340,y=20)
+
+    ch6 = Label(root, image= backImg)
+    ch6.place(x=360,y=20)
+
+if playerAmount >= 5:
+    ch7 = Label(root, image= backImg)
+    ch7.place(x=40,y=20)
+
+    ch7 = Label(root, image= backImg)
+    ch7.place(x=60,y=20)
 
 table1 = load(table, 1)
 t1 = Label(root, image=None)
-t1.place(x=20,y=150)
 
 table2 = load(table, 2)
 t2 = Label(root, image=None)
-t2.place(x=120,y=150)
 
 table3 = load(table, 3)
 t3 = Label(root, image=None)
-t3.place(x=220,y=150)
 
 table4 = load(table, 4)
 t4 = Label(root, image=None)
-t4.place(x=320,y=150)
 
 table5 = load(table, 5)
 t5 = Label(root, image=None)
-t5.place(x=420,y=150)
 
 drw = Button(root, text="Draw cards", width=10, bg="orange", command=clicked)
 drw.place(x=210, y=300)
@@ -287,7 +364,6 @@ txt.place(x=windowX/5*0+55,y=400)
 bet = Button(root, text="Bet", command=Bet, width=10)
 bet.place(x=windowX/5*0+55,y=425)
 betError = Label(root, text="", fg = "red")
-betError.place(x=1,y=375)
 
 check = Button(root, text="Check", command=Check, width=10)
 check.place(x=windowX/5*1+55,y=425)
@@ -299,6 +375,5 @@ fold = Button(root, text="Fold", command=Fold, width=10)
 fold.place(x=windowX/5*3+55,y=425)
 
 handScore = Label(root, text="")
-handScore.place(x=150,y=265)
 
 root.mainloop()
